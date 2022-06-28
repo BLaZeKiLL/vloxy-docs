1fdc413acb05c664859fc3ac1a5c29b101832eb9
Devashish Lal <laldevashish@gmail.com>
2022-06-28 13:07:10 +0530

    homepage image added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,15 +11,17 @@ function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/introduction">
-            Vloxy Engine - Getting Started
-          </Link>
+      <div className="card">
+        <div className="card__body">
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div className={styles.buttons}>
+            <Link
+              className="button button--primary button--lg"
+              to="/docs/introduction">
+              Vloxy Engine - Getting Started
+            </Link>
+          </div>
         </div>
       </div>
     </header>
@@ -27,15 +29,10 @@ function HomepageHeader() {
 }
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+    <Layout>
       <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
+      <HomepageFeatures />
     </Layout>
   );
 }
