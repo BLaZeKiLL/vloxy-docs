9c75b211eb78d5dd05033a0a0ea21e36debaca20
Devashish Lal <laldevashish@gmail.com>
2022-08-29 16:34:59 +0530

    docusaurus update, homepage chanegs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,20 +7,26 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import styles from './index.module.css';
 
+const Svg = require('@site/static/img/vloxy_logo.svg').default;
+
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="card">
         <div className="card__body">
-          <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
-          <div className={styles.buttons}>
-            <Link
-              className="button button--primary button--lg"
-              to="/docs/introduction">
-              Vloxy Engine - Getting Started
-            </Link>
+          <div className="row">
+            <Svg className={styles.logo} role="img" />
+            <div className={clsx('col', styles.vloxy_title)}>
+              <h1 className="hero__title">{siteConfig.title}</h1>
+              <div className={styles.buttons}>
+                <Link
+                  className="button button--primary button--lg"
+                  to="/docs/introduction">
+                  Get Started
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
       </div>
